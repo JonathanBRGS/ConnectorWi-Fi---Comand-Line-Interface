74c6aac87bf202c1836c73d18592b73d26d99607
Jonathan <jonathan.borges.araujo@outlook.com>
2024-10-04 17:47:52 -0300

    04OUT2014 1747

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ class Program
         Console.Write("Digite o nome da rede Wi-Fi (SSID): ");
         string ssid = Console.ReadLine();
 
-        char[] characters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', '/', '=', '*', '!', '@', '#', '_', '-', '%', '&', '(', ')', '+', }; // Você pode adicionar mais caracteres aqui
+        char[] characters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         
         // Define o comprimento máximo das combinações:
         
         int maxLength = 12;
 
-        for (int length = 1; length <= maxLength && !passwordCorrect; length++)
+        for (int length = 8; length <= maxLength && !passwordCorrect; length++)
         {
             foreach (var combination in GenerateCombinations(characters, "", length))
             {
